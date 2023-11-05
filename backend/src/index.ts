282d734678dec152f419e0cf81bc115a80a88abe
Chirag Ranasaria <90066944+Chirag9697@users.noreply.github.com>
2023-11-05 22:57:41 +0545

    added backend auth

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+//lib
+import express from 'express';
+import knex from "knex";
+import { Model } from 'objection';
+import cors from 'cors';
+import * as fromuser from './packages/users';
+import * as fromauth from './packages/authentication';
+import {development} from '../knexfile';
+
+export const app=express();
+// const development=require("../knexfile");
+const connection = development;
+var cors=require('cors');
+
+Model.knex(knex(connection));
+
+app.use(cors());
+app.use(express.json());
+
+const initial="api/v1";
+
+app.use(`/${initial}/auth`,fromauth.router);
+
+
+app.listen(3000,()=>{
+    console.log("listening on port 3000")
+})
+
+// module.exports=app;
